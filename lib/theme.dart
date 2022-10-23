a6042e3e8d6ebe23819a00cfa98a6b7bb24533a4
doni rifai <donirifai17@gmail.com>
2022-10-23 13:36:02 +0700

    make theme style

---
+++ lib/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = const Color(0xff6C5ECF);
+Color secondaryColor = const Color(0xff38ABBE);
+Color alertColor = const Color(0xffED6363);
+Color priceColor = const Color(0xff2C96F1);
+Color backgroundColor1 = const Color(0xff1F1D2B);
+Color backgroundColor2 = const Color(0xff2B2937);
+Color backgroundColor3 = const Color(0xff242231);
+Color primaryTextStyle = const Color(0xffF1F0F2);
+Color secondaryTextStyle = const Color(0xff999999);
+
+TextStyle primariTerxtStyle = GoogleFonts.poppins(
+  color: primaryTextStyle,
+);
+
+TextStyle secondariTextStyle = GoogleFonts.poppins(
+  color: secondaryTextStyle,
+);
+
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: priceColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;

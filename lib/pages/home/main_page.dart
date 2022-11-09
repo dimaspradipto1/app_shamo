aeed70f91e11e7753567e58a9152b9de93968f2a
dimas pradipto <dimaspradipto@gmail.com>
2022-11-09 12:29:52 +0700

    membuat custom bottom navigation

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,9 +1,72 @@
+import 'package:app_shamo/theme.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget cartButton() {
+      return FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: secondaryColor,
+        child: Image.asset(
+          'assets/icon_cart.png',
+          width: 20,
+        ),
+      );
+    }
+
+    Widget customBottomNav() {
+      return ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        child: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 10,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            backgroundColor: backgroundColor4,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_home.png',
+                  width: 21,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_chat.png',
+                  width: 20,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_wishlist.png',
+                  width: 20,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_profile.png',
+                  width: 18,
+                ),
+                label: '',
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
+      backgroundColor: backgroundColor1,
+      floatingActionButton: cartButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customBottomNav(),
       body: Center(
         child: Text(
           "Main Page",
--- lib/theme.dart
+++ lib/theme.dart
@@ -10,6 +10,7 @@ Color priceColor = const Color(0xff2C96F1);
 Color backgroundColor1 = const Color(0xff1F1D2B);
 Color backgroundColor2 = const Color(0xff2B2937);
 Color backgroundColor3 = const Color(0xff242231);
+Color backgroundColor4 = const Color(0xff252836);
 Color primaryTextColor = const Color(0xffF1F0F2);
 Color secondaryTextColor = const Color(0xff999999);
 Color subTitleColor = const Color(0xff504F5E);

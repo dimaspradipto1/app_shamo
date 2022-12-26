c253364e283fffb718ce2a66679f0090e8028653
dimas pradipto <dimaspradipto11@gmail.com>
2022-12-26 13:24:25 +0700

    detail chat bagian input

--- lib/pages/home/detail_caht_page.dart
+++ lib/pages/home/detail_caht_page.dart
@@ -46,9 +46,112 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    Widget productPreview() {
+      return Container(
+        width: 255,
+        height: 74,
+        margin: const EdgeInsets.only(bottom: 20),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColor5,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: primaryColor,
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISIO...',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/button_close.png',
+              width: 22,
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      color: backgroundColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Typle Message...',
+                          hintStyle: subTitleTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 45,
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
+      bottomNavigationBar: chatInput(),
     );
   }
 }

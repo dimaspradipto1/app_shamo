30e7f5e0b30654b8481f682b28fd4caea113e938
dimas pradipto <dimaspradipto11@gmail.com>
2022-12-27 11:51:51 +0700

    wishlist page empty

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -6,14 +6,87 @@ class WishListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor1,
-      body: Center(
-        child: Text(
-          "WishList Page",
-          style: primaryTextStyle,
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Favorite Shoes',
+          style: primaryTextStyle.copyWith(
+            fontWeight: medium,
+            fontSize: 18,
+          ),
         ),
-      ),
+        elevation: 0,
+      );
+    }
+
+    Widget emptyWishList() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/image_wishlist.png',
+                width: 74,
+              ),
+              const SizedBox(
+                height: 23,
+              ),
+              Text(
+                ' You don\'t have dream shoes?',
+                style: primaryTextStyle.copyWith(
+                  fontWeight: medium,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Let\'s find your favorite shoes',
+                style: secondaryTextStyle,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 44,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 10,
+                    ),
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        emptyWishList(),
+      ],
     );
   }
 }
